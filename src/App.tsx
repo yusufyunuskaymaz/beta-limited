e5727bb1ee50b426473cdb076aa38ab00c57e212
Yusuf Kaymaz <kaymazyusuf88@gmail.com>
2023-11-21 00:27:30 +0300

    first commit cards done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import Typography from "@mui/material/Typography";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import IconButton from "@mui/material/IconButton";
+import { Add, Minus, Star1 } from "iconsax-react";
+import Chip from "@mui/material/Chip";
 
 function App() {
+  const [data, setData] = useState<any[]>([]);
+  const fetchData = async () => {
+    const { data } = await axios.get(
+      "https://linkedin-cv-crawler.beta-limited.workers.dev/interview/products"
+    );
+    setData(data);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(data, "data");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack spacing={3} direction="row" sx={{ width: "70%", m: "auto", mt: 5 }}>
+      {data.map((item) => (
+        <Stack flexGrow={1} key={item.id}>
+          <Card sx={{position:"relative",height:"50vh"}}>
+
+          <Chip sx={{position:"absolute", top:10, left:5}} label={item.discount} color="error" />
+
+            <Stack sx={{ bgcolor: "#EEEEEE" }}>
+              <CardMedia
+                component="img"
+                height="194"
+                image={item.image}
+                alt="Paella dish"
+              />
+            </Stack>
+
+            <CardContent>
+              <Stack
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Stack spacing={1}>
+                  <Typography>{item.name}</Typography>
+                  <Stack direction="row">
+                    <Star1 size="20" color="#EFB746" variant="Bulk" />
+                    <Star1 size="20" color="#EFB746" variant="Bulk" />
+                    <Star1 size="20" color="#EFB746" variant="Bulk" />
+                    <Star1 size="20" color="#EFB746" variant="Bulk" />
+                    <Star1 size="20" color="#EFB746" variant="Bulk" />
+                  </Stack>
+                  <Stack direction="row" spacing={2}>
+                    <Typography sx={{color:"#C34A5A"}}>$ {item.price}</Typography>
+                    <Typography sx={{color:"#8F94A5",textDecoration:"line-through"}}>$ {item.originalPrice}</Typography>
+                  </Stack>
+                </Stack>
+                <Stack alignItems="center" spacing={1}>
+                  <IconButton sx={{ borderRadius: 1,border:1,borderColor:"#C34A5A",p:"2px" }} aria-label="delete">
+                    <Minus size="24" color="#C34A5A" />
+                  </IconButton>
+                  <Typography>5</Typography>
+                  <IconButton sx={{ borderRadius: 1,border:1,borderColor:"#C34A5A",p:"2px" }} aria-label="delete">
+                    <Add size="24" color="#C34A5A" />
+                  </IconButton>
+                </Stack>
+              </Stack>
+            </CardContent>
+          </Card>
+        </Stack>
+      ))}
+    </Stack>
   );
 }
 
